8954bb163c239bd47509e8b6f249393831dba620
Sebastian Rafal Smal <sebastian.smal@gmx.de>
2022-08-15 13:17:29 +0200

    Initial Commit

---
+++ ErsteIOsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErsteIOsApp
+//
+//  Created by Rafael on 15.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
